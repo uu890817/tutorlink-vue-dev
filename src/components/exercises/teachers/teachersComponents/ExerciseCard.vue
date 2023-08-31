50174304a46520ad3221e8b28d80a08de1071ef0
YuGe <uu890817@gmail.com>
2023-08-31 10:33:36 +0800

    新增分享試卷外部畫面

--- src/components/exercises/teachers/teachersComponents/ExerciseCard.vue
+++ src/components/exercises/teachers/teachersComponents/ExerciseCard.vue
@@ -41,7 +41,7 @@
                     批改試卷
                 </n-button>
             </a>
-            <n-button strong secondary type="info">
+            <n-button strong secondary type="info" @click="showModal = true">
                 <n-icon>
                     <MdPersonAdd />
                 </n-icon>
@@ -53,12 +53,12 @@
                 </n-icon>
                 更新習題
             </n-button>
-            <n-button strong secondary type="info">
+            <!-- <n-button strong secondary type="info">
                 <n-icon>
                     <MdClipboard />
                 </n-icon>
                 拷貝試卷
-            </n-button>
+            </n-button> -->
             <n-button strong secondary type="error" @click="onDelete">
                 <n-icon>
                     <MdTrash />
@@ -67,13 +67,30 @@
             </n-button>
         </n-space>
     </n-card>
+    <n-modal v-model:show="showModal" class="custom-card" preset="card" :style="bodyStyle" title="分享試卷" size="huge"
+        :bordered="false" :segmented="segmented">
+        <template #header-extra>
+            <n-input-group>
+                <n-input placeholder="請輸入ID或名字搜索" />
+                <!-- <n-button type="primary" ghost>
+                    搜尋
+                </n-button> -->
+            </n-input-group>
+        </template>
+        <shareExerciseCard></shareExerciseCard>
+        <template #footer>
+
+        </template>
+    </n-modal>
 </template>
 
 <script setup lang="js">
 import { MdHelpCircle, MdPersonAdd, MdClipboard, MdCheckmarkCircleOutline, MdSettings, MdTrash, MdHand } from '@vicons/ionicons4'
 
 import { ref, computed, h } from 'vue'
-import { useMessage, useDialog, useNotification, NIcon } from 'naive-ui'
+import { useDialog, useNotification, NIcon } from 'naive-ui'
+
+import shareExerciseCard from '@/components/exercises/teachers/teachersComponents/shareExerciseCard.vue'
 
 const props = defineProps({
     sId: Number
@@ -88,7 +105,6 @@ const qNa = computed(() => {
     return "/teacher/qa/" + props.sId
 })
 
-const message = useMessage()
 const dialog = useDialog()
 const notification = useNotification()
 
@@ -101,7 +117,6 @@ const onDelete = () => {
         maskClosable: false,
         icon: () => h(NIcon, null, [h(MdHand)]),
         onPositiveClick: () => {
-            message.success("已刪除");
             notification['success']({
                 content: "刪除成功",
                 meta: "拉進垃圾車",
@@ -110,7 +125,6 @@ const onDelete = () => {
             });
         },
         onNegativeClick: () => {
-            message.error("刪除失敗");
             notification['error']({
                 content: "刪除失敗",
                 meta: "太可惜了",
@@ -119,9 +133,16 @@ const onDelete = () => {
             });
         }
     });
-
 }
 
+const bodyStyle = {
+    width: "800px"
+}
+const segmented = {
+    content: "soft",
+    footer: "soft"
+}
+const showModal = ref(false)
 
 
 
@@ -134,7 +155,7 @@ const onDelete = () => {
 }
 
 .n-button {
-    padding: 10px 50px;
+    padding: 20px;
     margin-right: 10px;
     font-size: large;
 
---
+++ src/components/exercises/teachers/teachersComponents/shareExerciseCard.vue
@@ -0,0 +1,72 @@
+<template>
+    <n-card title="" hoverable>
+
+        <n-space justify="space-between" size="large" align="center">
+            <img class="stuImg" src="https://fakeimg.pl/300/" alt="" srcset="">
+            <h1>王同學</h1>
+            <n-space vertical>
+                <n-button type="primary" @click="showModal = true">已分享</n-button>
+                <n-button type="primary" @click="showModal = true">修改</n-button>
+            </n-space>
+
+        </n-space>
+    </n-card>
+</template>
+
+<script setup lang="js">
+import { MdDoneAll } from '@vicons/ionicons4'
+
+import { ref } from 'vue'
+
+import { NCard } from 'naive-ui';
+const showModal = ref(false)
+
+
+const onNegativeClick = () => {
+    showModal.value = false
+}
+
+
+const onPositiveClick = () => {
+    showModal.value = false
+    notify('success')
+
+
+
+}
+
+
+
+
+</script>
+
+<style scoped>
+.n-card {
+    margin-bottom: 10px;
+}
+
+.n-button {
+    padding: 10px 50px;
+    margin-right: 10px;
+    font-size: large;
+
+}
+
+.stuImg {
+    width: 100px;
+    height: 100px;
+    border-radius: 100%;
+}
+
+p {
+    color: yellowgreen;
+}
+
+.NProgress {
+    margin-right: 80px;
+}
+
+.n-progress {
+    max-width: 100px;
+}
+</style>
